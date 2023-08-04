ead91ff324f410f8bce85ca4a99bf24db3744d90
Agassi <84224519+DevAgassi@users.noreply.github.com>
2023-08-04 14:37:14 +0300

    Develop (#3)
    * Add graphql,prisma and users feature
    
    * add next(web), graphql-codegen
    
    * add Git CI
    
    * add env. for web and api
    
    * add i18n

---
+++ apps/web/app/[lang]/page.tsx
@@ -0,0 +1,15 @@
+import { gql } from '../../data-access/graphq-client';
+import styles from './page.module.css';
+
+
+export default async function Index() {
+  const { users } = await gql.GetUsers();
+
+  return (
+    <div className={styles.page}>
+      {users.map((user) => (
+        <div key={user.id}>Name: {user.name}</div>
+      ))}
+    </div>
+  );
+}
